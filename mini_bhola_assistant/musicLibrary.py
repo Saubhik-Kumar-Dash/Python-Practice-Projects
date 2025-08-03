df2e4828aed5984d6fe41c2dccb0804e2d6ce03d
Saubhik Kumar <103452749+Saubhik-Kumar-Dash@users.noreply.github.com>
2025-08-03 12:19:24 +0530

    Add files via upload

---
+++ mini_bhola_assistant/musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "romantic": "https://www.youtube.com/watch?v=aVHfeUDGWKE&pp=ygUJbmFhbXVua2lu",
+    "english": "https://www.youtube.com/watch?v=JgDNFQ2RaLQ&ab_channel=EdSheeran",
+    "indipop": "https://www.youtube.com/watch?v=sUf2PtEZris&ab_channel=SanjuRathodSR",
+    "latest": "https://www.youtube.com/watch?v=rcPuG_PkAf8&ab_channel=T-Series"
+}
